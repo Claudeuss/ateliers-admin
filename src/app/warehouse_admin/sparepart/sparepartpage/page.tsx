3c44f894211cd7ef3e013e3c8f10e92ceb033e03
Claudeuss <133555372+Claudeuss@users.noreply.github.com>
2023-12-01 10:35:23 +0700

    Update page.tsx
    Update :
    - Add read data for filter stock item

--- src/app/warehouse_admin/sparepart/sparepartpage/page.tsx
+++ src/app/warehouse_admin/sparepart/sparepartpage/page.tsx
@@ -1,43 +1,74 @@
 'use client'
-import Sidebar from '@/components/sidebar'
 import SidebarGudang from '@/components/sidebar_gudang'
 import Stockitem from '@/components/stockitem'
-import { QuerySnapshot, collection, doc, getDocs } from 'firebase/firestore'
+import { QuerySnapshot, collection, deleteDoc, doc, getDocs } from 'firebase/firestore'
 import React, { useEffect, useState } from 'react'
-import { BiSearch } from 'react-icons/bi'
+import { BiEdit, BiSearch } from 'react-icons/bi'
 import { BsBoxSeam, BsBoxes, BsTrash3 } from 'react-icons/bs'
 import { SlSocialDropbox } from 'react-icons/sl'
 import { db } from '../../../../../lib/firebase/page'
+import { useRouter } from 'next/navigation'
 
-const [sparepart, setSparepart] = useState<any[]>([])
-const itemCollectionRef = collection(db, 'sparepart')
-
-useEffect(
-    () => {
-        const getSparepart = async () => {
-            try {
-                const data: QuerySnapshot = await getDocs(itemCollectionRef);
-                setSparepart(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-            } catch (error) {
-                console.error('Error', error);
-            }
-        };
-        getSparepart();
-    }, []
-);
+interface Sparepart {
+    id: string;
+    name: string;
+    price: number;
+    category: string;
+    quantity: number;
+}
 
 const sparepartpage = () => {
+    const [spareparts, setSparepart] = useState<any[]>([]);
+    const itemCollectionRef = collection(db, 'sparepart')
+    const router = useRouter();
+    useEffect(
+        () => {
+            const getSparepart = async () => {
+                try {
+                    const data: QuerySnapshot = await getDocs(itemCollectionRef);
+                    setSparepart(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+                } catch (error) {
+                    console.error('Error fetching spareparts:', error);
+                }
+            };
+
+            getSparepart();
+        }, []
+    );
+
+    // Delete Item Function
+    const handleDelete = async (id: string) => {
+        console.log('Deleting item with ID:', id);
+
+        try {
+            // Delete the document with the specified ID
+            await deleteDoc(doc(db, 'sparepart', id));
+
+            // Log after deletion
+            console.log('Item deleted. Updating local state.');
+
+            // Update the local state to remove the deleted item
+            setSparepart((prevSpareparts) => prevSpareparts.filter((item) => item.id !== id));
+        } catch (error) {
+            console.error('Error deleting data:', error);
+        }
+    };
+
+    const handleUpdateClick = (id: any) => {
+        router.push(`/warehouse_admin/sparepart/edit?id=${id}`);
+    }
+
     return (
         <>
             <SidebarGudang />
-            <div className=' w-screen pl-28 bg-[#EAEAEA]'>
+            <div className=' w-full pl-28 bg-[#EAEAEA] overflow-hidden'>
                 <div className=' p-5'>
                     <h1 className=' text-2xl font-semibold'>Sparepart</h1>
                     <div className='px-5 py-3'>
                         <div className=' grid grid-cols-3 gap-4 pb-4'>
-                            <Stockitem icon={<BsBoxes className=" text-white m-auto text-4xl" />} title={'Total Stock'} total={'1000'} />
-                            <Stockitem icon={<BsBoxSeam className=" text-white m-auto text-4xl" />} title={'Low Stock'} total={'100'} />
-                            <Stockitem icon={<SlSocialDropbox className=" text-white m-auto text-4xl" />} title={'Empty Stock'} total={'10'} />
+                            <Stockitem icon={<BsBoxes className=" text-white m-auto text-4xl" />} title={'Total Stock'} total={spareparts.filter((item) => item.quantity >= 1).length} />
+                            <Stockitem icon={<BsBoxSeam className=" text-white m-auto text-4xl" />} title={'Low Stock'} total={spareparts.filter((item) => item.quantity < 10 && item.quantity >= 1).length} />
+                            <Stockitem icon={<SlSocialDropbox className=" text-white m-auto text-4xl" />} title={'Empty Stock'} total={spareparts.filter((item) => item.quantity === 0).length} />
                         </div>
                         <div className=' w-full h-[400px] py-2 bg-white rounded-md'>
                             <div className=' flex px-5 justify-between'>
@@ -52,7 +83,7 @@ const sparepartpage = () => {
                                     <input type="text" name="search" id="" className=' w-full px-2 bg-transparent border-none outline-none' />
                                 </div>
                             </div>
-                            <div className=' mt-2'>
+                            <div className=' mt-2 h-[340px] overflow-y-scroll'>
                                 <table className=' table border-t-2 border-black  w-full'>
                                     <thead className=' border-b'>
                                         <tr>
@@ -64,17 +95,26 @@ const sparepartpage = () => {
                                             <th className=' '>Action</th>
                                         </tr>
                                     </thead>
-                                    <tbody className=' '>
+                                    <tbody className=''>
 
-                                        {sparepart.map((item) => (
-                                            <tr>
-                                                <td className='w-10 py-1 text-center border-b border-r'>1</td>
-                                                <td className='w-64 py-1 text-center border-b border-r truncate text-ellipsis line-clamp-1 px-2'>{item.name}</td>
+                                        {spareparts.map((item, index) => (
+                                            <tr key={item.id} className=''>
+                                                <td className='w-10 py-1 text-center border-b border-r'>{index + 1}</td>
+                                                <td className='w-64 py-1 text-center border-b border-r overflow-hidden whitespace-nowrap px-2'>{item.name}</td>
                                                 <td className=' w-64 py-1 text-center border-b border-r'>{item.price}</td>
                                                 <td className=' w-56 py-1 text-center border-b border-r'>{item.category}</td>
                                                 <td className=' w-56 py-1 text-center border-b border-r'>{item.quantity}</td>
-                                                <td className=' py-1 text-center border-b'>
-                                                    <BsTrash3 className=' text-xl m-auto' />
+                                                <td className=' px-2 py-2 border-b flex'>
+                                                    <div className='w-full h-full hover:bg-blue-800 hover:text-white py-1 rounded-md'
+                                                        onClick={() => handleUpdateClick(item.id)}
+                                                    >
+                                                        <BiEdit className='mx-auto text-xl ' />
+                                                    </div>
+                                                    <div className='w-full h-full hover:bg-red-500 hover:text-white py-1 rounded-md'
+                                                        onClick={() => handleDelete(item.id)}
+                                                    >
+                                                        <BsTrash3 className='mx-auto text-xl ' />
+                                                    </div>
                                                 </td>
                                             </tr>
                                         ))}

f93d360a265a0f1ea2fedbe06c0174b6fe236d3f
Claudeuss <133555372+Claudeuss@users.noreply.github.com>
2024-01-19 10:40:19 +0700

    Update page.tsx
    make page super admin

--- src/app/SuperAdmin/page.tsx
+++ src/app/SuperAdmin/page.tsx
@@ -1,10 +1,21 @@
+import ChartCard from '@/components/ChartCard'
+import SidebarGudang from '@/components/sidebar_gudang'
 import React from 'react'
+
 
 const page = () => {
   return (
     <>
-    <div>
-        
+    <SidebarGudang/>
+    <div className='h-full w-screen pl-28 bg-[#EAEAEA] overflow-x-hidden'>
+        <div className='p-5 overflow-y-auto overflow-x-hidden'>
+          <p className='text-2xl font-semibold'>
+            Admin
+          </p>
+          <div>
+            <ChartCard/>
+          </div>
+        </div>
     </div>
     </>
   )

436b8b244e94d31ad0a54e2351d81d4ad24ca2c6
Claudeuss <133555372+Claudeuss@users.noreply.github.com>
2024-01-30 08:56:45 +0700

    Update ChartCard.tsx
    commit

--- src/components/ChartCard.tsx
+++ src/components/ChartCard.tsx
@@ -7,44 +7,31 @@ import { Doughnut } from 'react-chartjs-2';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const DoughnutChartComponent: React.FC = () => {
-  // Data untuk chart doughnut
+  // Data for the doughnut chart
   const data = {
     labels: ['Red', 'Blue'],
     datasets: [
       {
-        data: [12, 19],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.6)',
-          'rgba(54, 162, 235, 0.6)',
-          // 'rgba(255, 206, 86, 0.6)',
-          // 'rgba(75, 192, 192, 0.6)',
-          // 'rgba(153, 102, 255, 0.6)',
-        ],
+        data: [20, 20],
+        backgroundColor: ['#9498FF', '#1B24FF'],
       },
     ],
   };
 
-  // Opsi konfigurasi untuk chart doughnut
+  // Configuration options for the doughnut chart
   const options = {
     responsive: true,
     maintainAspectRatio: false,
-    width: 200,
-    height: 200,
-    plugins: {}
+    plugins: {},
   };
 
   return (
-    
-      <div className=' h-[400px] w-[400px] flex'>
-        <div className="w-96 h-96 max-w-lg mx-auto mt-8 bg-white p-4 rounded-lg shadow-md ">
+    <div className='h-[400px] w-[400px] flex'>
+      <div className="w-96 h-96 max-w-lg mx-auto mt-8 bg-white p-4 rounded-lg shadow-md">
         <h2 className="text-lg font-semibold mb-4">Doughnut Chart</h2>
-        <Doughnut 
-          data={data} 
-          options={options} 
-        />
+        <Doughnut data={data} options={options} />
       </div>
-      </div>
-    
+    </div>
   );
 };
 

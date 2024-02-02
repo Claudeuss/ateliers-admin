c92fce0b4d6ca9b305413c6bbdbf8ba2a24ad55a
Claudeuss <133555372+Claudeuss@users.noreply.github.com>
2024-02-02 09:26:17 +0700

    Update page.tsx
    add table view

--- src/app/SuperAdmin/page.tsx
+++ src/app/SuperAdmin/page.tsx
@@ -1,12 +1,31 @@
 'use client'
 import ChartCard from '@/components/ChartCard'
 import SidebarSuper from '@/components/sidebar_super'
-import React from 'react'
+import { QuerySnapshot, collection, getDocs } from 'firebase/firestore'
+import React, { useEffect, useState } from 'react'
+import { db } from '../../../lib/firebase/page'
 
 
 const page: React.FC = () => {
   const data = [30, 20, 25, 15, 10];
-  const labels = ['Label 1', 'Label 2', 'Label 3', 'Label 4', 'Label 5'];
+  const labels = ['Label 1', 'Label 2'];
+
+  const [cart, setCart] = useState<any[]>([]);
+    const itemCollectionRef = collection(db, 'transactions');
+
+    useEffect(() => {
+        const getCart = async () => {
+            try {
+                const data: QuerySnapshot = await getDocs(itemCollectionRef);
+                setCart(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+            } catch (error) {
+                console.error('Error fetching spareparts:', error);
+            }
+        };
+
+        getCart();
+    }, []);
+    
   return (
     <>
     <SidebarSuper/>
@@ -15,8 +34,47 @@ const page: React.FC = () => {
           <p className='text-2xl font-semibold'>
             Admin
           </p>
-          <div className='p-5 flex'>
+          <div>
+          <div className='p-5 flex gap-4'>
             <ChartCard data={data} labels={labels}/>
+            <div className='flex flex-col flex-grow h-[300px]'>
+                <table className='table w-full overflow-y-auto bg-white rounded-md h-[300px]'>
+                  <thead>
+                    <tr className=' border-b border-slate-500'>
+                      <th className='w-10 py-2'>Id</th>
+                      <th className='w-1/3 py-2'>Date</th>
+                      <th className='w-1/3 py-2'>Product Name</th>
+                      <th className='w-44 py-2'>Quantity</th>
+                      <th className='w-52 py-2'>Edit</th>
+                    </tr>
+                  </thead>
+                  <tbody className='overflow-auto flex-grow'>
+                    {cart.map((item, index) => (
+                      <tr className=' border-b' key={item.id}>
+                        <td className='text-center border-r'>{index + 1}</td>
+                          <td className=' text-center border-r px-2'>
+                            {new Date(item.timestamp.seconds * 1000).toLocaleString('en-GB', {
+                              weekday: 'long',
+                              year: 'numeric',
+                              month: 'long',
+                              day: 'numeric',
+                              hour: 'numeric',
+                              minute: 'numeric',
+                          })}
+                        </td>
+                        <td className=' text-center border-r px-2'>{item.name}</td>
+                        <td className=' text-center border-r'>{item.quantity}</td>
+                        <td className='px-2 py-2'>
+                            <div className='w-full h-full hover:bg-red-500 hover:text-white py-1 rounded-md'>
+                                
+                            </div>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+              </div>
           </div>
         </div>
     </div>

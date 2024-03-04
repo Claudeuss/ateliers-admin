9548e9cc77b7f75494646b9ba5b587b0e4120f49
Sitinurzanah <143475955+Sitinurzanah@users.noreply.github.com>
2024-03-04 11:51:18 +0700

    Update sidebar_gudang.tsx
    change size image logo

--- src/components/sidebar_gudang.tsx
+++ src/components/sidebar_gudang.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react';
 import { BiHomeAlt2 } from 'react-icons/bi';
 import { MdOutlineLogout } from 'react-icons/md';
@@ -5,12 +6,12 @@ import { LiaToolsSolid } from 'react-icons/lia';
 import { RxDashboard } from "react-icons/rx";
 import { PiUserCircleLight } from "react-icons/pi";
 import { BiTransferAlt } from "react-icons/bi";
+
 const SidebarGudang = () => {
     return (
         <div className='py-7 px-5 w-28 h-screen fixed bg-white z-40 top-0 '>
             <div className='px-2 flex flex-col justify-center items-center mb-10'>
-                <img src="assets/images/7.png" alt="" />
-
+                <img src="../assets/images/7.png" alt="" height={100} width={100} />
             </div>
 
             <div>

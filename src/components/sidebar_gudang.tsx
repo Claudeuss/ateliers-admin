5c2142c4489124cf4bfb0f982b1ae113e438a603
Sitinurzanah <143475955+Sitinurzanah@users.noreply.github.com>
2024-03-04 12:00:58 +0700

    Update sidebar_gudang.tsx
    c

--- src/components/sidebar_gudang.tsx
+++ src/components/sidebar_gudang.tsx
@@ -11,7 +11,7 @@ const SidebarGudang = () => {
     return (
         <div className='py-7 px-5 w-28 h-screen fixed bg-white z-40 top-0 '>
             <div className='px-2 flex flex-col justify-center items-center mb-10'>
-                <img src="../assets/images/7.png" alt="" height={100} width={100} />
+                <img src="assets/images/7.png" alt="" height={50} width={50} />
             </div>
 
             <div>

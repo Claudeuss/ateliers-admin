bcb13cf739de85f47c75c835a8d2aaec4bfcd4d2
Claudeuss <133555372+Claudeuss@users.noreply.github.com>
2024-03-08 11:40:34 +0700

    Update LoginForm.tsx
    c

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -70,7 +70,7 @@ const RegisterForm = () => {
                     </div>
                     <div className="mb-4">
                         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                            Email
+                            Emails
                         </label>
                         <input
                             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
